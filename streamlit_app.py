372e4d123a2043365b06ae82ebbb55ea09bbed61
priyakalidossvellore <priya-3.k-3@cognizant.com>
2026-05-01 22:23:41 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,6 +19,7 @@ st.write(
 
 cnx=st.connection("snowflake")
 session = cnx.session()
+
 my_dataframe = session.table("smoothies.public.fruit_options").select (col('search_on'))
 st.dataframe(data=my_dataframe,use_container_width=True)
 st.stop()
