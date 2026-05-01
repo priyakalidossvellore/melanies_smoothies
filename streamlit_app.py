54de37d0983edceec6f614039ac469e48b379739
priyakalidossvellore <priya-3.k-3@cognizant.com>
2026-05-01 21:27:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,6 +32,8 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string+=fruit_chosen + ' '
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ fruit_chosen )    
+        sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
 
     st.write(ingredients_string)
 
@@ -47,9 +49,6 @@ if time_to_insert:
     
     st.success('Your Smoothie is ordered!,'+title,icon="✅")
 
-#New section to display smoothiefroot nutrition information
 import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-# st.text(smoothiefroot_response.json())    
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
 
